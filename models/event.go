6992946c9f9d78a1595964c72c74eabf0af94e9f
runquan-ray-zhou <151119924+runquan-ray-zhou@users.noreply.github.com>
2024-12-29 17:18:25 -0500

    Update Save function in event.go to include query

--- models/event.go
+++ models/event.go
@@ -7,10 +7,14 @@ custom event struct
 
 package models
 
-import "time"
+import (
+	"time"
+
+	"github.com/runquan-ray-zhou/udemy-event-booking-rest-api/db"
+)
 
 type Event struct {
-	ID          int       // id of event
+	ID          int64     // id of event
 	Name        string    `binding: "required"`
 	Description string    `binding: "required"`
 	Location    string    `binding: "required"`
@@ -21,10 +25,26 @@ type Event struct {
 var events = []Event{} // slice of events
 
 // methods to interact with the events
-func (e Event) Save() { // save method to save event to database
+func (e Event) Save() error { // save method to save event to database
+	query := `
+	INSERT INTO events(name, description, location, dateTime, user_id) 
+	VALUES (?, ?, ?, ?, ?)`
+	stmt, err := db.DB.Prepare(query)
 
-	events = append(events, e)
+	if err != nil {
+		return err
+	}
 
+	defer stmt.Close() // will close whenever this function ends
+	result, err := stmt.Exec(e.Name, e.Description, e.Location, e.DateTime, e.UserID)
+
+	if err != nil {
+		return err
+	}
+
+	id, err := result.LastInsertId()
+	e.ID = id
+	return err
 }
 
 // normal function
