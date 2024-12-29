c3e2b37c45aa598b9cc27bf5866612f3e1b46814
runquan-ray-zhou <151119924+runquan-ray-zhou@users.noreply.github.com>
2024-12-29 15:12:23 -0500

    Add POST route Add createEvent function to handle post request Install RestClient vscode extension to check routes

--- main.go
+++ main.go
@@ -4,12 +4,14 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/runquan-ray-zhou/udemy-event-booking-rest-api/models"
 )
 
 func main() {
 	server := gin.Default() // configures http server that comes with middleware, a server pointer
 
-	server.GET("/events", getEvents) // register a handler for incoming get request
+	server.GET("/events", getEvents)    // register a handler for incoming get request
+	server.POST("/events", createEvent) // post request
 
 	server.Run(":8080") // start listening to incoming request when main func is executed, currently on local host 8080
 
@@ -17,5 +19,19 @@ func main() {
 
 // send back a response
 func getEvents(context *gin.Context) { // gin will pass a context parament to the function, if you set this handler as this endpoint.
-	context.JSON(http.StatusOK, gin.H{"message": "Hello!"}) // send back a response in JSON format, pass back a http status code and data
+	events := models.GetAllEvents()
+	context.JSON(http.StatusOK, events) // send back a response in JSON format, pass back a http status code and data
+}
+
+func createEvent(context *gin.Context) { // extract of data from request
+	var event models.Event
+	err := context.ShouldBindJSON(&event) // similar to Scan() function, store that request data into event, must follow structure of Event
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse request data."}) // response message if there is error
+	}
+
+	event.ID = 1
+	event.UserID = 1
+	context.JSON(http.StatusCreated, gin.H{"message": "Event created:", "event": event}) // response message when event is created successful
 }
--- models/event.go
+++ models/event.go
@@ -10,12 +10,12 @@ package models
 import "time"
 
 type Event struct {
-	ID          int // id of event
-	Name        string
-	Description string
-	Location    string
-	DateTime    time.Time
-	UserID      int //links the even to the user who created it
+	ID          int       // id of event
+	Name        string    `binding: "required"`
+	Description string    `binding: "required"`
+	Location    string    `binding: "required"`
+	DateTime    time.Time `binding: "required"`
+	UserID      int       // links the even to the user who created it
 }
 
 var events = []Event{} // slice of events
@@ -25,4 +25,9 @@ func (e Event) Save() { // save method to save event to database
 
 	events = append(events, e)
 
+}
+
+// normal function
+func GetAllEvents() []Event { // call it to get available event
+	return events // return events slice
 }
