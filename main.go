729b3c69299af41d891af3c427f5ca3522d60e02
runquan-ray-zhou <151119924+runquan-ray-zhou@users.noreply.github.com>
2024-12-29 14:34:09 -0500

    Set up initial /events GET route Set localhost to port 8080 Add getEvents function to handle /events GET route with status code OK/200 Add response to /events GET route with response as a Map

--- main.go
+++ main.go
@@ -1,11 +1,21 @@
 package main
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	server := gin.Default() // configures http server that comes with middleware, a server pointer
 
-	gin.Default() // configures http server that comes with middleware
+	server.GET("/events", getEvents) // register a handler for incoming get request
 
+	server.Run(":8080") // start listening to incoming request when main func is executed, currently on local host 8080
+
+}
+
+// send back a response
+func getEvents(context *gin.Context) { // gin will pass a context parament to the function, if you set this handler as this endpoint.
+	context.JSON(http.StatusOK, gin.H{"message": "Hello!"}) // send back a response in JSON format, pass back a http status code and data
 }
