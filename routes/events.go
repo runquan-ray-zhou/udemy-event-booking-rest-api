64898af24ea9565b443dc64608400d2f15c75671
runquan-ray-zhou <151119924+runquan-ray-zhou@users.noreply.github.com>
2024-12-30 01:58:05 -0500

    Add delete route, delete method, and delete event function

--- routes/events.go
+++ routes/events.go
@@ -91,3 +91,29 @@ func updateEvent(context *gin.Context) {
 
 	context.JSON(http.StatusOK, gin.H{"message": "Event updated successfully!"})
 }
+
+// delete event
+func deleteEvent(context *gin.Context) {
+	eventId, err := strconv.ParseInt(context.Param("id"), 10, 64) // get path parameter value
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse event id."})
+		return
+	}
+
+	event, err := models.GetEventByID(eventId)
+
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not fetch the event."})
+		return
+	}
+
+	err = event.Delete()
+
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not delete the event."})
+		return
+	}
+
+	context.JSON(http.StatusOK, gin.H{"message": "Event deleted successfully!"})
+
+}
--- routes/routes.go
+++ routes/routes.go
@@ -4,8 +4,9 @@ import "github.com/gin-gonic/gin"
 
 func RegisterRoutes(server *gin.Engine) {
 	// routes, part of the same package
-	server.GET("/events", getEvents)       // register a handler for incoming get request
-	server.GET("/events/:id", getEvent)    // get events by id
-	server.POST("/events", createEvent)    // post request
-	server.PUT("/events/:id", updateEvent) // update request
+	server.GET("/events", getEvents)          // register a handler for incoming get request
+	server.GET("/events/:id", getEvent)       // get events by id
+	server.POST("/events", createEvent)       // post request
+	server.PUT("/events/:id", updateEvent)    // update request
+	server.DELETE("/events/:id", deleteEvent) // delete request
 }
