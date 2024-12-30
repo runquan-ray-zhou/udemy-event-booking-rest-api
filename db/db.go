21558e1cd86dc2bab072e4d1d8828d2c57c5c11d
runquan-ray-zhou <151119924+runquan-ray-zhou@users.noreply.github.com>
2024-12-30 08:49:16 -0500

    Add new function to create users table Update createEventsTable query to include foreign key of user id

--- db/db.go
+++ db/db.go
@@ -25,6 +25,20 @@ func InitDB() {
 
 // create table
 func createTables() {
+	createUsersTable := `
+	CREATE TABLE IF NOT EXISTS users (
+		id INTEGER PRIMARY KEY AUTOINCREMET,
+		email TEXT NOT NULL UNIQUE,
+		password TEXT NOT NULL
+	)
+	`
+
+	_, err := DB.Exec(createUsersTable)
+
+	if err != nil {
+		panic("Could not create users table.")
+	}
+
 	createEventsTables := `
 	CREATE TABLE IF NOT EXISTS events (
 		id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -33,9 +47,10 @@ func createTables() {
 		location TEXT NOT NULL,
 		dateTime DATETIME NOT NULL,
 		user_id INTEGER
+		FOREIGN KEY(user_id) REFERENCES users(id)
 	)
 	`
-	_, err := DB.Exec(createEventsTables) // executes sql query statement
+	_, err = DB.Exec(createEventsTables) // executes sql query statement
 
 	if err != nil {
 		panic("Could not create events table.")
--- routes/routes.go
+++ routes/routes.go
@@ -9,4 +9,5 @@ func RegisterRoutes(server *gin.Engine) {
 	server.POST("/events", createEvent)       // post request
 	server.PUT("/events/:id", updateEvent)    // update request
 	server.DELETE("/events/:id", deleteEvent) // delete request
+	server.POST("/signup")                    // new user route
 }
