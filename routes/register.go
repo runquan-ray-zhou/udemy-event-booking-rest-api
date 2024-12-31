acf7a6b980c0e7578f6d6e7de8fbf2da75b31506
runquan-ray-zhou <151119924+runquan-ray-zhou@users.noreply.github.com>
2024-12-30 22:02:12 -0500

    Add CancelRegistration method to Event type struct Complete cancelRegistration handler function in register.go Add cancelRegistration handler function to DELETE route

--- routes/register.go
+++ routes/register.go
@@ -36,5 +36,18 @@ func registerForEvent(context *gin.Context) {
 }
 
 func cancelRegistration(context *gin.Context) {
+	userId := context.GetInt64("userId")
+	eventId, err := strconv.ParseInt(context.Param("id"), 10, 64) // get path parameter value
 
+	var event models.Event
+	event.ID = eventId
+
+	err = event.CancelRegistration(userId)
+
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not cancel registration."})
+		return
+	}
+
+	context.JSON(http.StatusOK, gin.H{"message": "Cancelled!"})
 }
